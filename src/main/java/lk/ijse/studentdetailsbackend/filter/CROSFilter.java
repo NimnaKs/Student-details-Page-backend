a8d943833323ec174310599cc5d1afad11e1a0d2
NimnaKs <nimnakaveesha123456@gmail.com>
2023-12-28 14:34:33 +0530

    Filter done

--- src/main/java/lk/ijse/studentdetailsbackend/filter/CROSFilter.java
+++ src/main/java/lk/ijse/studentdetailsbackend/filter/CROSFilter.java
@@ -1,4 +1,26 @@
 package lk.ijse.studentdetailsbackend.filter;
 
-public class CROSFilter {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/*")
+public class CROSFilter extends HttpFilter {
+
+    @Override
+    protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
+        String origin = req.getHeader("Origin");
+        if(origin != null && origin.contains(getServletContext().getInitParameter("origin"))){
+            res.setHeader("Access-Control-Allow-Origin",origin);
+            res.setHeader("Access-Control-Allow-Methods","GET,POST,PUT,DELETE,HEADER");
+            res.setHeader("Access-Control-Allow-Headers","Content-Type");
+            res.setHeader("Access-Control-Expose-Headers","Content-Type");
+        }
+        chain.doFilter(req,res);
+    }
 }
